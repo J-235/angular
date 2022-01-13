1352c50d7e3ee1ed21c05ac27a475a10335b5f10
J-235 <jonas.leiter@gmx.de>
2022-01-13 11:48:11 +0100

    finalized example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of as observableOf } from 'rxjs';
+import { Observable, of as observableOf } from 'rxjs';
 import { concatMap, delay, mergeMap } from 'rxjs/operators';
 
 @Component({
@@ -14,21 +14,41 @@ export class AppComponent {
     // Because concatMap does not subscribe to the next observable until the previous completes,
     // the value from the source delayed by 2000ms will be emitted first.
 
-    // mergeMap subscribes immediately to inner observables, the observable with the lesser delay (1000ms) will emit, 
+    // mergeMap subscribes immediately to all inner observables, the observable with the lesser delay (1000ms) will emit, 
     // followed by the observable which takes 2000ms to complete.
 
     const source = observableOf(2000, 1000);
-
-    // output: With concatMap: Delayed by: 2000ms, With concatMap: Delayed by: 1000ms
+    
+    // output: With concatMap: Delayed by: 4010ms, With concatMap: Delayed by: 2010ms
     const concatMapExample = source
-      .pipe(concatMap(val => observableOf(`Delayed by: ${val}ms`)
-      .pipe(delay(val))))
-      .subscribe(val => console.log(`With concatMap: ${val}`));
-
+    .pipe(concatMap(val => observableOf(`Delayed by: ${val}ms`)
+    .pipe(delay(val))))
+    .subscribe(val => console.log(`With concatMap: ${val}`));
+    
     // output: With mergeMap: Delayed by: 1000ms, With mergeMap: Delayed by: 2000ms
     const mergeMapExample = source
-      .pipe(mergeMap(val => observableOf(`Delayed by: ${val}ms`)
-      .pipe(delay(val))))
-      .subscribe(val => console.log(`With mergeMap: ${val}`));
+    .pipe(mergeMap(val => observableOf(`Delayed by: ${val}ms`)
+    .pipe(delay(val))))
+    .subscribe(val => console.log(`With mergeMap: ${val}`));
+
+    // output: With concatMap: calculated delay: 4005ms, With concatMap: calculated delay: 2005ms
+    const multipleConcatMapExample = source
+      .pipe(concatMap(val => this.double(val)),
+            concatMap(val => this.addOffset(val)),
+            concatMap(val => this.substractOffset(val)),
+            concatMap(val => observableOf(`calculated delay: ${val}ms`)))
+      .subscribe(val => console.log(`With concatMap: ${val}`));
+  }
+
+  private double(val: number): Observable<number> {
+    return observableOf(val * 2);
+  }
+
+  private addOffset(val: number): Observable<number> {
+    return observableOf(val + 10);
+  }
+
+  private substractOffset(val: number): Observable<number> {
+    return observableOf(val - 5);
   }
 }
