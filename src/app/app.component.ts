a7ec24c35f661f73fbbc5263b132e8748baee758
J-235 <jonas.leiter@gmx.de>
2022-01-27 17:07:56 +0100

    drag drop in tables with async dataSource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatTable } from '@angular/material/table';
 
+interface Todo {
+  id: number,
+  description: string,
+  deadline: string
+}
+
+const defaultTodo = {
+  id: 0,
+  description: '',
+  deadline: ''
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playground';
+
+  columnsCurrent = ['description', 'deadline'];
+  private currentTodoCache: Todo[] = [];
+  private currentTodoDataSource: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([]);
+  public currentTodoDataSource$: Observable<Todo[]> = this.currentTodoDataSource.asObservable();
+
+  columnsBacklog = ['description', 'deadline'];
+  private todoBacklogCache: Todo[] = [];
+  private todoBacklogDataSource: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([]);
+  public todoBacklogDataSource$: Observable<Todo[]> = this.todoBacklogDataSource.asObservable();
+
+  // @ViewChild(MatTable) currentTodoTable: MatTable<Todo>;
+  @ViewChildren(MatTable) allTables: QueryList<MatTable<Todo>>;
+
+  ngOnInit() {
+    this.currentTodoCache.push(
+      { ...defaultTodo, description: 'T1'}, 
+      { ...defaultTodo, description: 'T2'}, 
+      { ...defaultTodo, description: 'T3'}, 
+    );
+    this.currentTodoDataSource.next(this.currentTodoCache);
+    this.todoBacklogCache.push(
+      { ...defaultTodo, description: 'T4'}, 
+      { ...defaultTodo, description: 'T5'}, 
+      { ...defaultTodo, description: 'T6', deadline: '11.11.1111'}, 
+    );
+    this.todoBacklogDataSource.next(this.todoBacklogCache);
+  }
+
+  onDrop(event: CdkDragDrop<Todo[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.currentTodoCache, event.previousIndex, event.currentIndex);
+    } else {
+      copyArrayItem(
+        this.todoBacklogCache,
+        this.currentTodoCache,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      
+    }
+    // this.currentTodoTable.renderRows();
+    for(let table of this.allTables) {
+      table.renderRows();
+    }
+    console.log('current', this.currentTodoCache)
+    console.log('backlog', this.todoBacklogCache)
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
