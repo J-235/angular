c68f37fa15257c8897046e4bd55e6cf5dc3c2179
J-235 <jonas.leiter@gmx.de>
2022-02-14 17:01:17 +0100

    first example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+interface NameDeadline {
+  name: string,
+  deadline: Date
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playground';
+
+  columnsToDisplay = ['name', 'deadline'];
+
+
+  dataSource: NameDeadline[] = [
+    { name: 'test', deadline: new Date()},
+    { name: 'test', deadline: new Date()},
+    { name: 'test', deadline: new Date()},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'de' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
