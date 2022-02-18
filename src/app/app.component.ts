2f73b5fe339838c087bba72c2c67d0c9fcda8eca
J-235 <jonas.leiter@gmx.de>
2022-02-18 09:04:53 +0100

    edited comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
     const date = new Date(); // Fri Feb 18 2022 08:33:40 GMT+0100 (Mitteleuropäische Normalzeit)
     const dateToBackend = date.toISOString(); // 2022-02-18T07:33:40.030Z 
     const dateFromBackend = new Date(dateToBackend); // Fri Feb 18 2022 08:33:40 GMT+0100 (Mitteleuropäische Normalzeit)
+    // but string returned from backend is: '2022-02-18 07:33:40.030'
 
     
     // Postgres Format TIMESTAMP ignores timezone offset,
@@ -31,10 +32,13 @@ export class AppComponent implements OnInit {
     const timeZoneOffset = date.getTimezoneOffset(); // UTC - GMT+1  = -60
     const extension = (timeZoneOffset / -60).toString().padStart(3, '+0'); // +01
 
-    // Angular to Postres: '2004-10-19 10:23:54+02'
+    // Angular: '2004-10-19T10:23:54Z'
     const postgresTimestamp = dateToBackend.replace('Z', extension);
-    // Postgres to Angular: '2004-10-19 10:23:54Z' (UTC, present it to users local timezone by using JS .toLocalDate() and other methods)
+    // Angular to Postres: '2004-10-19T10:23:54+02'
+    
+    // Postgres: '2004-10-19 10:23:54+02'
     const postgresToAngular = new Date(postgresTimestamp.slice(0, -3).concat('Z')); // '2004-10-19 10:23:54+02' >> '2004-10-19 10:23:54Z'
+    // Postgres to Angular: '2004-10-19 10:23:54Z' (UTC, present it to users local timezone by using JS .toLocalDate() and other methods)
 
 
     console.log(date);
